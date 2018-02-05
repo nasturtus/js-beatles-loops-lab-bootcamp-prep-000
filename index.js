3dfbb90a7d1ab4d16faced4b210bbad87553d6e2
Santosh SN <nasturtus@gmail.com>
2018-02-05 04:01:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ function theBeatlesPlay(musicians, instruments) {
   for (var i = 0; i < musicians.length; i++) {
     musiciansInstruments.push(`${musicians[i]} plays ${instruments[i]}`);
   }
+  return musiciansInstruments
 }
